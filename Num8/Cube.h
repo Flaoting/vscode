315f2546531d0ad1c0bf572ec9c2a5793d70998f
Flaoting <568936545@qq.com>
2020-04-07 17:52:45 +0800

    second

---
+++ Num8/Cube.h
@@ -0,0 +1,56 @@
+
+#ifndef Cube_H_
+
+#define Cube_H_
+
+#include "Rect.h"
+
+class Cube : public Rect{
+
+    private:
+
+        float height;
+
+    public:
+
+        float getArea();
+
+        Cube (float w,float l,float h);
+
+        Cube();
+
+    	float getLen( )    { return  length;  }
+
+   	    float getWidth( ){ return  width;  }
+
+        float getHeight( ){ return  height;  }
+
+};
+
+float Cube ::getArea(){
+
+    float a=width*length;
+
+    float b=width*height;
+
+    float c=length*height;
+
+    area=2*(a+b+c);
+
+    Rect ::getArea();
+
+}
+
+Cube::Cube (float w,float l,float h):Rect(w,l){
+
+    height=h;
+
+}
+
+Cube :: Cube():Rect(){
+
+    height=0.0f;
+
+}
+
+#endif
---
+++ Num8/mian.cpp
@@ -0,0 +1,39 @@
+/*要求:1）在Rect类基础之上公有派生出长方体类Cube，
+
+新增一个私有数据成员：高度height，
+
+并设计函数成员中getArea函数，覆盖Rect中的同名虚函数，用于计算长方体的表 面积。
+
+2）设计main函数，对以上的两个类进行测试，
+
+其中cube类中getArea函数要求使用基类Rect的指针通过赋值兼容规则进行调用。
+
+*/
+
+#include "Rect.h"
+
+#include "Cube.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    Rect *p;
+
+    p =new Rect(2,3);
+
+    cout <<p->getArea()<<endl;
+
+
+
+    Cube a(1,2,3);
+
+    p=&a;
+
+    cout<<p->getArea()<<endl;
+
+    return 0;
+
+}
